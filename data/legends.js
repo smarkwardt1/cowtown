8c449ed9f440dd50e8c9eb9519a1e9b0249f6f95
Shane Markwardt <shane.markwardt@fort-companies.com>
2023-05-08 15:09:27 -0500

    modals and markdown

---
+++ data/legends.js
@@ -0,0 +1,25 @@
+const legends = [
+    {
+      name: "Bob Wills",
+      image_src: "/images/legends/BobWills_Legends.png",
+      link: "/bob-wills",
+    },
+    {
+      name: "Milton Brown",
+      image_src: "/images/legends/MiltonBrown_Legends.png",
+      link: "/milton-brown"
+    },
+    {
+      name: "Tommy Duncan",
+      image_src: "/images/legends/TommyDuncan_Legends.png",
+      link: "tommy-duncan"
+    },
+    {
+      name: "Bob Dunn",
+      image_src: "/images/legends/BobDunn_Legends.png",
+      link: "bob-dunn"
+    },
+  ];
+  
+  export default legends;
+  
