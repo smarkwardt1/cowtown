6803a1f989266237551f8375548774db6d7aea59
Shane Markwardt <shane.markwardt@fort-companies.com>
2023-05-08 17:23:54 -0500

    modal work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
