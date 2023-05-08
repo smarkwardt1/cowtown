100d11ec6365d1f06cc29c81f90af9bc222df3ae
Shane Markwardt <shane.markwardt@fort-companies.com>
2023-05-08 16:09:29 -0500

    adding aws bucket

--- data/legends.js
+++ data/legends.js
@@ -1,23 +1,23 @@
 const legends = [
     {
       name: "Bob Wills",
-      image_src: "/images/legends/BobWills_Legends.png",
+      image_src: "https://cbws.s3.us-east-2.amazonaws.com/images/legends/BobWills_Legends.png",
       link: "/bob-wills",
     },
     {
       name: "Milton Brown",
-      image_src: "/images/legends/MiltonBrown_Legends.png",
+      image_src: "https://cbws.s3.us-east-2.amazonaws.com/images/legends/MiltonBrown_Legends.png",
       link: "/milton-brown"
     },
     {
       name: "Tommy Duncan",
-      image_src: "/images/legends/TommyDuncan_Legends.png",
-      link: "tommy-duncan"
+      image_src: "https://cbws.s3.us-east-2.amazonaws.com/images/legends/TommyDuncan_Legends.png",
+      link: "/tommy-duncan"
     },
     {
       name: "Bob Dunn",
-      image_src: "/images/legends/BobDunn_Legends.png",
-      link: "bob-dunn"
+      image_src: "https://cbws.s3.us-east-2.amazonaws.com/images/legends/BobDunn_Legends.png",
+      link: "/bob-dunn"
     },
   ];
   
