ea10825ef49fd054089c1fb1058731ac2e8bad5d
Shane Markwardt <shane.markwardt@fort-companies.com>
2023-05-09 16:26:47 -0500

    festival page looking good

---
+++ data/tickets.js
@@ -0,0 +1,40 @@
+const tickets = [
+  {
+    name: "3-Day Ultra-VIP Tickets",
+    text: "Best Reserved VIP Seating + 3 Delicious Dinners",
+    price: "$200",
+    paypal: "",
+  },
+  {
+    name: "3-Day VIP Tickets",
+    text: "Best Reserved Seating Close to the Dance Floor",
+    price: "$150",
+    paypal: "",
+  },
+  {
+    name: "3-Day Reserved Tickets",
+    text: "Reserved Seating in Corner Sections of the Hall",
+    price: "$100",
+    paypal: "",
+  },
+  {
+    name: "3-Day Wall Seat Tickets",
+    text: "Open Seating Around the Walls of the Hall (No Table)",
+    price: "$75",
+    paypal: "",
+  },
+  {
+    name: "3-Day Dinner Tickets",
+    text: "Best Reserved VIP Seating + 3 Delicious Dinners",
+    price: "$50",
+    paypal: "",
+  },
+  {
+    name: "Stockyard Tour Tickets",
+    text: "4 Hour Bus Tour - Shopping, Cattle Drive & Dance",
+    price: "$10",
+    paypal: "",
+  },
+];
+
+export default tickets;
